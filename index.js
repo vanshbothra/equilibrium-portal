c28447397e354ad5dc0ab7ce58a719eba6f22ab2
vanshbothra <67708601+vanshbothra@users.noreply.github.com>
2024-02-24 09:56:30 +0530

    final changes

--- index.js
+++ index.js
@@ -17,10 +17,24 @@ require('dotenv').config();
 //     console.log(participant.College);
 // }
 
-fs.readFile("./equilibrium_registrations.json", 'utf-8', (err, data) => {
-    const participants = JSON.parse(data);
-    console.log(participants[0]["Candidate's Name"]);
-});
+// fs.readFile("./equilibrium_registrations.json", 'utf-8', async (err, data) => {
+//     const participants = JSON.parse(data);
+//     for(let i=0; i<(participants.length); i++) {
+//         // console.log(participants[i]["Candidate's Name"]);
+//         var participant = {
+//             Name: participants[i]["Candidate's Name"],
+//             College: participants[i]["College"],
+//             Contact: participants[i]["Mobile Number"]
+//         };
+//         // console.log(participant);
+//         try {
+//             const response = await axios.post(`${apiUrl}/participants`,{data: participant}, axiosConfig);
+//             console.log('Data posted to Strapi:', i);
+//         } catch (error) {
+//             console.error('Error posting data to Strapi:', error.message);
+//         }
+//     }
+// });
 
 const updateSheet = async (participantDetails) => {
     const auth = new google.auth.GoogleAuth({
@@ -48,6 +62,32 @@ const updateSheet = async (participantDetails) => {
     });
 };
 
+const updateSheetOut = async (participantDetails) => {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "equilibrium-24-414611-fe327b7599b4.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    const authClientObject = await auth.getClient();
+
+    const googleSheetsInstance = google.sheets({
+        version: "v4",
+        auth: authClientObject
+    });
+
+    const spreadsheetId = "1YW4a-kMYc5iKdgUf126kAX7-fVlAktX3Z6ZXkc_KJYo";
+
+    await googleSheetsInstance.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "Sheet2",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [participantDetails]
+        }
+    });
+};
+
 const axiosConfig = {
     headers: {
       'Content-Type': 'application/json',
@@ -60,8 +100,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/:id", async (req, res) => {
-    var response = await axios.get(`${apiUrl}/participants/${req.params.id}?populate=events`, axiosConfig);
-    console.log(response.data.data.attributes.events.data);
+    var adjID = Number(req.params.id) + 25;
+    var response = await axios.get(`${apiUrl}/participants/${adjID}?populate=events`, axiosConfig);
+    // console.log(response.data.data.attributes.events.data);
     res.render('index', {details: response.data.data});
 });
 
@@ -69,8 +110,8 @@ app.post("/sign-in", async (req, res) => {
     console.log(req.body);
     var participantId = req.body.participantId;
     var participantDetails = await axios.get(`${apiUrl}/participants/${participantId}`, axiosConfig);
-    var detail = [participantDetails.data.data.attributes.Name, participantDetails.data.data.attributes.Contact, participantDetails.data.data.attributes.College, "in"];
-    console.log(participantDetails.data);
+    var detail = [participantDetails.data.data.attributes.Contact, participantDetails.data.data.attributes.Name, participantDetails.data.data.attributes.College, "in"];
+    // console.log(participantDetails.data);
     try {
         var signIn = await axios.put(`${apiUrl}/participants/${participantId}`, {data: {Day_1: true}} , axiosConfig);
         res.redirect(`/${participantId}`);
@@ -85,10 +126,12 @@ app.post("/sign-out", async (req, res) => {
     console.log(req.body);
     var participantId = req.body.participantId;
     var participantDetails = await axios.get(`${apiUrl}/participants/${participantId}`, axiosConfig);
+    var detailOut = [participantDetails.data.data.attributes.Contact, participantDetails.data.data.attributes.Name, participantDetails.data.data.attributes.College, "out"];
     // console.log(participantDetails.data);
     try {
         var signOut = await axios.put(`${apiUrl}/participants/${participantId}`, {data: {Day_1: false}} , axiosConfig);
         res.redirect(`/${participantId}`);
+        updateSheetOut(detailOut);
     } catch (error) {
         console.error("Error Signing: ", error);
         res.status(500).send("Error while signing.");
